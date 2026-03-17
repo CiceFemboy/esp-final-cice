d3b384eba6c772a12cbcc8a3786714a31ecc4822
Cice <cutebussy@proton.me>
2026-03-17 11:40:53 -0400

    Update installer.py
    updated the installer script so it could automatically add the packages paths on the virtual environment directly. now installs correctly all the packages ith pip being recognized by pythonas a module.

--- installer.py
+++ installer.py
@@ -1,6 +1,12 @@
-import sys
 import os
-import subprocess
-exec(open(os.getcwd() + "\\get-pip.py").read())
+DEFAULT_PATH = os.getcwd()
+print(f"[DEBUG] installing pip")
+with open(DEFAULT_PATH + "\\python-3.13.12-embed-amd64\\python313._pth", "a") as file:
+    file.write("\nLib\nLib\site-packages")
+    file.close()
+exec(open(DEFAULT_PATH + "\\get-pip.py").read())
 print(f"[DEBUG] installing requirements")
-subprocess.run([sys.executable, "-m", "pip", "install","-r", "requirements.txt"])
+import subprocess
+import sys
+import pip
+subprocess.run([sys.executable, "-m", "pip", "install","-r", DEFAULT_PATH + "\\requirements.txt"])
