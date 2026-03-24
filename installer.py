217485321a54314ae6c8a8a723bc6f8594325d54
Cice <cutebussy@proton.me>
2026-03-24 11:29:09 -0400

    Updated installer script
    added capabilities to install 'demomunk' as a module on the virtual environment

--- installer.py
+++ installer.py
@@ -2,7 +2,7 @@ import os
 DEFAULT_PATH = os.getcwd()
 print(f"[DEBUG] installing pip")
 with open(DEFAULT_PATH + "\\python-3.13.12-embed-amd64\\python313._pth", "a") as file:
-    file.write("\nLib\nLib\site-packages")
+    file.write("\nLib\nLib\site-packages\nInclude\pygame\nLib\ESP-scripts")
     file.close()
 exec(open(DEFAULT_PATH + "\\get-pip.py").read())
 print(f"[DEBUG] installing requirements")
@@ -10,3 +10,7 @@ import subprocess
 import sys
 import pip
 subprocess.run([sys.executable, "-m", "pip", "install","-r", DEFAULT_PATH + "\\requirements.txt"])
+import shutil
+print(f"[DEBUG] installing demomunk")
+os.mkdir(DEFAULT_PATH + "\\python-3.13.12-embed-amd64\\Lib\\ESP-scripts")
+shutil.copyfile(DEFAULT_PATH + "\\ESP-scripts\\demomunk.py", DEFAULT_PATH + "\\python-3.13.12-embed-amd64\\Lib\\ESP-scripts\\demomunk.py")
