068cdcbbd48c7f17979599d30bcbae7484ab7f6a
Cice <cutebussy@proton.me>
2026-03-24 12:26:24 -0400

    Add files via upload

--- installer.py
+++ installer.py
@@ -2,7 +2,7 @@ import os
 DEFAULT_PATH = os.getcwd()
 print(f"[DEBUG] installing pip")
 with open(DEFAULT_PATH + "\\python-3.13.12-embed-amd64\\python313._pth", "a") as file:
-    file.write("\nLib\nLib\site-packages\nInclude\pygame\nLib\ESP-scripts")
+    file.write("\nLib\nLib\site-packages\nInclude\pygame")
     file.close()
 exec(open(DEFAULT_PATH + "\\get-pip.py").read())
 print(f"[DEBUG] installing requirements")
@@ -12,5 +12,5 @@ import pip
 subprocess.run([sys.executable, "-m", "pip", "install","-r", DEFAULT_PATH + "\\requirements.txt"])
 import shutil
 print(f"[DEBUG] installing demomunk")
-os.mkdir(DEFAULT_PATH + "\\python-3.13.12-embed-amd64\\Lib\\ESP-scripts")
-shutil.copyfile(DEFAULT_PATH + "\\ESP-scripts\\demomunk.py", DEFAULT_PATH + "\\python-3.13.12-embed-amd64\\Lib\\ESP-scripts\\demomunk.py")
+os.mkdir(DEFAULT_PATH + "\\python-3.13.12-embed-amd64\\Lib\\site-packages\\demomunk")
+shutil.copyfile(DEFAULT_PATH + "\\ESP-scripts\\demomunk.py", DEFAULT_PATH + "\\python-3.13.12-embed-amd64\\Lib\\site-packages\\demomunk\\Game.py")
---
+++ launcher.py
@@ -0,0 +1,4 @@
+import os
+
+DEFAULT_PATH = os.getcwd()
+exec(open("neat_munk.py").read())
